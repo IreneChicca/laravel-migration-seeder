50a04585ac4bb37f12d67b5b7c184fb13dca9458
IreneChicca <irene.chicca@outlook.com>
2023-10-16 15:50:47 +0200

    Add table col

--- database/migrations/2023_10_16_123955_create_trains_table.php
+++ database/migrations/2023_10_16_123955_create_trains_table.php
@@ -15,6 +15,17 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('corporate', 200);
+            $table->string('start_station', 50);
+            $table->string('end_station', 50);
+            $table->time('start_time');
+            $table->time('end_time');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('train_code', 10);
+            $table->tinyInteger('n_wagon');
+            $table->boolean('delay')->default(false);
+            $table->boolean('deleted')->dafault(false);
             $table->timestamps();
         });
     }
